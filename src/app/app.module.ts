3c7cff882d1d0fe3573860cab07841e87a687cbc
Shwshank <shwshank3@gmail.com>
2018-04-13 14:26:38 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SubmitAreaComponent } from './home/submit-area/submit-area.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    SubmitAreaComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/home/submit-area/submit-area.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { NgModel } from '@angular/forms';
+
+@Component({
+  selector: 'app-submit-area',
+  templateUrl: './submit-area.component.html',
+  styleUrls: ['./submit-area.component.scss']
+})
+export class SubmitAreaComponent implements OnInit {
+
+  imageArray: any =[];
+  url: any = "";
+  formData = new FormData();
+  files: any;
+  name: any;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  getVal($event) {
+
+    this.files = $event.target.files || $event.srcElement.files;
+    let file = this.files[0];
+    let reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = (event:any) => {
+      this.url = event.target.result;
+      this.pushToImageArray(this.url);
+    }
+
+  }
+
+  pushToImageArray(urlBase64) {
+
+    let i = this.imageArray.length +1;
+
+    this.imageArray.push({id:i, url:urlBase64});
+    console.log(this.imageArray);
+  }
+
+}
